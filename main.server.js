14b3acbe1cb5888d9fd8fa5e16f78ccef02be540
Elanis <elanis@hotmail.com>
2022-11-25 17:26:52 +0100

    Refacto serverside

--- main.server.js
+++ main.server.js
@@ -15,99 +15,109 @@ export default class FocusStats {
 		clearInterval(FocusStats.interval);
 	}
 
-	static async update() {
-		try {
-			const dbEntries = (await Database.execQuery('SELECT date, duration, exe, name FROM focus_stats')).rows;
-			const dbEntriesMapped = {};
-			for(const dbEntry of dbEntries) {
-				dbEntriesMapped[dbEntry.date.getTime()] = dbEntry;
-			}
+	static async pushHistory() {
+		log('Saving current focus stats ...', 'info');
 
-			const history 	= JSON.parse(fs.readFileSync(config['dataFolder'] + 'history.json'));
-			for(const name in history) {
-				for(const exe in history[name]) {
-					if(name === '' && exe === '') {
-						continue;
-					}
+		const dbEntries = (await Database.execQuery('SELECT date, duration, exe, name FROM focus_stats')).rows;
+		const dbEntriesMapped = {};
+		for(const dbEntry of dbEntries) {
+			dbEntriesMapped[dbEntry.date.getTime()] = dbEntry;
+		}
 
-					for(const element of history[name][exe]) {
-						const date = new Date(element.date);
-
-						if(typeof dbEntriesMapped[date.getTime()] !== 'undefined') {
-							if(
-								dbEntriesMapped[date.getTime()].duration === element.duration &&
-								dbEntriesMapped[date.getTime()].name === name &&
-								dbEntriesMapped[date.getTime()].exe === exe) {
-
-								continue;
-							}
-
-							await Database.execQuery(
-								'DELETE FROM focus_stats WHERE date = $1',
-								[date]
-							);
-						}
-
-						const [query, values] = Database.buildInsertQuery('focus_stats', {
-							name,
-							exe,
-							date,
-							duration: element.duration,
-						});
-
-						await Database.execQuery(
-							query,
-							values
-						);
-					}
-				}
-			}
-
-			log('Saved current focus stats', 'info');
-			log('Saving tags focus stats assignements', 'info');
-
-			// Get already inserted tags
-			const data = (await Database.execQuery('SELECT * FROM focus_stats_tags')).rows;
-
-			// TODO: delete data that has been removed on UI
-			// We won't do it now because there's now way to remove tags from UI atm
-
-			const dbTagsMapped = {};
-			for(let row of data) {
-				if(!dbTagsMapped[row.name]) {
-					dbTagsMapped[row.name] = {};
-				}
-				if(!dbTagsMapped[row.name][row.exe]) {
-					dbTagsMapped[row.name][row.exe] = [];
+		const history 	= JSON.parse(fs.readFileSync(config['dataFolder'] + 'history.json'));
+		for(const name in history) {
+			for(const exe in history[name]) {
+				if(name === '' && exe === '') {
+					continue;
 				}
 
-				dbTagsMapped[row.name][row.exe].push(row.tag);
-			}
+				for(const element of history[name][exe]) {
+					const date = new Date(element.date);
 
-			// Insert tags
-			const tags 		= JSON.parse(fs.readFileSync(config['dataFolder'] + 'tags.json'));
-			for(const name in tags) {
-				for(const exe in tags[name]) {
-					for(const tag of tags[name][exe]) {
-						if(dbTagsMapped[name] && dbTagsMapped[name][exe] && dbTagsMapped[name][exe].includes(tag)) {
+					if(typeof dbEntriesMapped[date.getTime()] !== 'undefined') {
+						if(
+							dbEntriesMapped[date.getTime()].duration === element.duration &&
+							dbEntriesMapped[date.getTime()].name === name &&
+							dbEntriesMapped[date.getTime()].exe === exe) {
+
 							continue;
 						}
 
-						const [query, values] = Database.buildInsertQuery('focus_stats_tags', {
-							name,
-							exe,
-							tag
-						});
-
 						await Database.execQuery(
-							query,
-							values
+							'DELETE FROM focus_stats WHERE date = $1',
+							[date]
 						);
 					}
+
+					const [query, values] = Database.buildInsertQuery('focus_stats', {
+						name,
+						exe,
+						date,
+						duration: element.duration,
+					});
+
+					await Database.execQuery(
+						query,
+						values
+					);
 				}
 			}
+		}
 
-			log('Saved tags focus stats assignements', 'info');
+		log('Saved current focus stats', 'info');
+	}
+
+	static async pushTags() {
+		log('Saving tags focus stats assignements', 'info');
+
+		// Get already inserted tags
+		const data = (await Database.execQuery('SELECT * FROM focus_stats_tags')).rows;
+
+		// TODO: delete data that has been removed on UI
+		// We won't do it now because there's now way to remove tags from UI atm
+
+		const dbTagsMapped = {};
+		for(let row of data) {
+			if(!dbTagsMapped[row.name]) {
+				dbTagsMapped[row.name] = {};
+			}
+			if(!dbTagsMapped[row.name][row.exe]) {
+				dbTagsMapped[row.name][row.exe] = [];
+			}
+
+			dbTagsMapped[row.name][row.exe].push(row.tag);
+		}
+
+		// Insert tags
+		const tags 		= JSON.parse(fs.readFileSync(config['dataFolder'] + 'tags.json'));
+		for(const name in tags) {
+			for(const exe in tags[name]) {
+				for(const tag of tags[name][exe]) {
+					if(dbTagsMapped[name] && dbTagsMapped[name][exe] && dbTagsMapped[name][exe].includes(tag)) {
+						continue;
+					}
+
+					const [query, values] = Database.buildInsertQuery('focus_stats_tags', {
+						name,
+						exe,
+						tag
+					});
+
+					await Database.execQuery(
+						query,
+						values
+					);
+				}
+			}
+		}
+
+		log('Saved tags focus stats assignements', 'info');
+	}
+
+	static async update() {
+		try {
+			await FocusStats.pushHistory();
+			await FocusStats.pushTags();
 		} catch(e) {
 			console.error(e);
 		}
