6edd5abc2575e3b4f53cad9fa15c902b22bf18b3
Elanis <elanis@hotmail.com>
2023-05-18 10:09:28 +0200

    Improve history persistance
    Using a temporary cache will prevent having whole history in RAM the whole time and prevent CPU spikes every 30 seconds when having a big history

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import getActiveProgram from './getActiveProgram.cjs';
 
-let history = {};
-if(AppDataManager.exists('focus-stats', 'history')) {
-	history = AppDataManager.loadObject('focus-stats', 'history');
+let historyCache = [];
+if(AppDataManager.exists('focus-stats', 'historyCache')) {
+	historyCache = AppDataManager.loadObject('focus-stats', 'historyCache');
 }
 if(!AppDataManager.exists('focus-stats', 'tags')) {
 	AppDataManager.saveObject('focus-stats', 'tags', {});
 }
-
-let lastActivityExe = '';
-let lastActivityName = '';
-let lastActivityId = '';
+if(!AppDataManager.exists('focus-stats', 'history')) {
+	AppDataManager.saveObject('focus-stats', 'history', {});
+}
 
 const config = ConfigManager.get('focus-stats', 'cleaner');
 
@@ -38,7 +37,7 @@ function cleanString(str) {
 	return str.substring(0, 1000);
 }
 
-function addNewActivityToHistory() {
+function addNewActivityToHistoryCache() {
 	const currProgramExe  = cleanString(getActiveProgram.getActiveProgramExe());
 	const currProgramName = cleanString(getActiveProgram.getActiveProgramName());
 
@@ -50,29 +49,61 @@ function addNewActivityToHistory() {
 	//console.log(new Date(), 'New activity:', currProgramName, getActiveProgram.getActiveProgramExe());
 	//console.log(new Date(), 'Exe:', getActiveProgram.getActiveProgramExe());
 
-	if(!history[currProgramExe]) {
-		history[currProgramExe] = {};
-	}
-	if(!history[currProgramExe][currProgramName]) {
-		history[currProgramExe][currProgramName] = [];
-	}
-
-	if(lastActivityExe === currProgramExe && lastActivityName === currProgramName) {
-		history[currProgramExe][currProgramName][lastActivityId].duration++;
+	const lastActivity = historyCache[historyCache.length - 1] || {};
+	if(lastActivity.name === currProgramName && lastActivity.exe === currProgramExe) {
+		historyCache[historyCache.length - 1].duration++;
 	} else {
-		lastActivityExe  = currProgramExe;
-		lastActivityName = currProgramName;
-
-		lastActivityId = history[currProgramExe][currProgramName].push({
+		historyCache.push({
+			name: currProgramName,
+			exe: currProgramExe,
 			date: new Date(),
 			duration: 0,
-		}) - 1;
+		});
 	}
-
 }
 
-setInterval(addNewActivityToHistory, 1000);
+function persistCache() {
+	AppDataManager.saveObject('focus-stats', 'historyCache', historyCache);
+}
 
-setInterval(() => {
+function persistFullData() {
+	const history = AppDataManager.loadObject('focus-stats', 'history');
+
+	while(historyCache.length > 0) {
+		const item = historyCache.pop();
+
+		if(!history[item.exe]) {
+			history[item.exe] = {};
+		}
+
+		if(!history[item.exe][item.name]) {
+			history[item.exe][item.name] = [];
+		}
+
+		if(history[item.exe][item.name].find(x => ((new Date(x.date)).getTime()) === ((new Date(item.date)).getTime()))) { // Duplicate
+			continue;
+		}
+
+		console.log('push data to history');
+
+		history[item.exe][item.name].push({
+			date: item.date,
+			duration: item.duration
+		});
+	}
+
+	console.log('Saving persistance ... ' + (new Date()).toISOString());
+
 	AppDataManager.saveObject('focus-stats', 'history', history);
-}, 30000);
+	persistCache();
+
+	console.log('Saved persistance ! ' + (new Date()).toISOString());
+
+	console.log('Persisted, length:' + historyCache.length);
+	console.log('Cache after persistance, length:' + historyCache.length);
+}
+
+setInterval(addNewActivityToHistoryCache, 1000);
+
+setInterval(persistCache, 30 * 1000);
+setInterval(persistFullData, 60 * 60 * 1000);
